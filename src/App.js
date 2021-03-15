3490aed69ab05a777aeb073031a12ff6f52dc8ae
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-03-15 23:32:17 +0200

    btn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import FormField from "./components/FormField";
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Expense Track</h1>
+      <h2>Add A New Item:</h2>
+      <FormField/>
     </div>
   );
 }
---
+++ src/components/FormField.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const FormField = () => {
+  const [inputField, setInputField] = useState('');
+  const [inputField2, setInputField2] = useState('');
+  const [inputField3, setInputField3] = useState('');
+
+  const handleInputs = ()=>{
+
+  }
+
+  return (
+    <div >
+      <form className='form'
+       onSubmit={e=>{e.preventDefault()}}>
+        <div>
+        <div className='text'>
+            <div className='inputtext'>Name:
+            <input className='nameinput'
+                type='text' 
+                placeholder='Where was the expense made?'
+                value={inputField}
+                onChange={e=>{setInputField(e.target.value)}}/>
+            </div>
+                
+            <div className='dateamount'>
+                <div>Date:</div>
+                    <input className='inputDate' type='date'
+                    value={inputField2}
+                    onChange={e=>{setInputField2(e.target.value)}}/>
+                <div>Amount:</div>
+                    <input className='inputnumber' type='number'
+                    value={inputField3}
+                    onChange={e=>{setInputField3(e.target.value)}}/>
+            </div>
+        </div>
+        <button
+         onClick={handleInputs}
+         type='submit'>Add Expense</button>
+        </div>
+      </form>
+      <div>
+         
+      </div>
+    </div>
+  );
+};
+
+export default FormField;
