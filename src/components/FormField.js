834fff0bf6a6a2e6c05be3352b2a042217eaedf6
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-03-16 17:52:34 +0200

    Update FormField.js

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,36 +1,52 @@
 import React, { useState } from "react";
 
 const FormField = () => {
-  const [inputField, setInputField] = useState('');
-  const [inputField2, setInputField2] = useState('');
-  const [inputField3, setInputField3] = useState('');
+  const [value, setInputField] = useState('');
+  const [value2, setInputField2] = useState('');
+  const [value3, setInputField3] = useState('');
+ 
+
+  const [printData,setPrintdata] = useState([]);
 
   const handleInputs = ()=>{
-    setInputField('');setInputField2('');setInputField3('')
+    const printData = [value,value2,value3];
+    setPrintdata(printData);
+    const divprint=()=>{ 
+      printData.map(item=><div>{item}</div>)
+      
+    }
+    setInputField('');setInputField2('');setInputField3('');
+}
+
+  const handleSubmit = (e)=>{
+      e.preventDefault();
+      handleInputs(printData);
+      console.log(printData)
   }
 
   return (
     <div >
       <form className='form'
-       onSubmit={e=>{e.preventDefault()}}>
+       onSubmit={handleSubmit}>
         <div>
         <div className='text'>
             <div className='inputtext'>Name:
             <input className='nameinput'
                 type='text' 
                 placeholder='Where was the expense made?'
-                value={inputField}
+                value={value}
                 onChange={e=>{setInputField(e.target.value)}}/>
             </div>
-                
             <div className='dateamount'>
                 <div>Date:</div>
                     <input className='inputDate' type='date'
-                    value={inputField2}
+                    value={value2}
                     onChange={e=>{setInputField2(e.target.value)}}/>
                 <div>Amount:</div>
-                    <input className='inputnumber' type='number'
-                    value={inputField3}
+                    <input className='inputnumber'
+                    placeholder='Enter amount'
+                    type='number'
+                    value={value3}
                     onChange={e=>{setInputField3(e.target.value)}}/>
             </div>
         </div>
@@ -40,7 +56,7 @@ const FormField = () => {
         </div>
       </form>
       <div>
-         
+         <div>{printData[0]}</div>
       </div>
     </div>
   );
